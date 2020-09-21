df2245c0b3cdbcd13c4561ace9dc09d3fbf8bd58
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 01:35:18 +0300

    update

--- port/build.msvc.make.cmd
+++ port/build.msvc.make.cmd
@@ -18,5 +18,5 @@ echo "Error: %ACTION%"
 exit 1
 :StepX
 
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-random.static.compile.info
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-random.dynamic.compile.info
+call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-random.static.compile
+call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-random.dynamic.compile
--- port/build.ubuntu.make.sh
+++ port/build.ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-random.static.compile.info
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-random.dynamic.compile.info
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-random.static.compile
+cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-random.dynamic.compile
