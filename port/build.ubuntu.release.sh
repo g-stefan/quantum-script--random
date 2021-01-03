c78d7397e499aefde81551c8ee248bb1bd1c786f
Grigore Stefan <g_stefan@yahoo.com>
2021-01-03 04:02:49 +0200

    release build

--- port/build.ubuntu.release.sh
+++ port/build.ubuntu.release.sh
@@ -3,11 +3,11 @@
 # http://unlicense.org/
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo "-> local-release quantum-script-extension-random"
+echo "-> release quantum-script-extension-random"
 
 cmdX(){
 	if ! "$@" ; then
-		echo "Error: local-release"
+		echo "Error: release"
 		exit 1
 	fi
 }
